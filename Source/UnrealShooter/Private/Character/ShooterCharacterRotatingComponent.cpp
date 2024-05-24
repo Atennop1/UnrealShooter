79bbc0ca8de4f970aba30ea236451e184797e87a
AtennopVer <anton7185569@gmail.com>
2024-05-24 12:35:01 +0300

    Made better head rotating

--- Source/UnrealShooter/Private/Character/ShooterCharacterRotatingComponent.cpp
+++ Source/UnrealShooter/Private/Character/ShooterCharacterRotatingComponent.cpp
@@ -1,7 +1,6 @@
 ﻿// Copyright Atennop and Krypton. All Rights Reserved.
 
 #include "Character/ShooterCharacterRotatingComponent.h"
-
 #include "Character/ShooterCharacter.h"
 
 UShooterCharacterRotatingComponent::UShooterCharacterRotatingComponent()
@@ -28,4 +27,11 @@ void UShooterCharacterRotatingComponent::Rotate(const FVector2D Input) const
 {
 	Character->AddControllerYawInput(Input.X);
 	Character->AddControllerPitchInput(Input.Y);
+	
+	FVector CharacterForwardVector = Character->GetActorRotation().Vector();
+	FVector CameraForwardVector = CharacterCamera->GetComponentRotation().Vector();
+	CharacterForwardVector.Z = CameraForwardVector.Z = 0;
+
+	if (const float Angle = FMath::RadiansToDegrees(FMath::Atan2(CameraForwardVector.Y * CharacterForwardVector.X - CameraForwardVector.X * CharacterForwardVector.Y, CameraForwardVector.X * CharacterForwardVector.X + CameraForwardVector.Y * CharacterForwardVector.Y)); FMath::Abs(Angle) > 45)
+		Character->SetActorRotation(FRotator(0, Character->GetControlRotation().Yaw + 45 * (Angle > 0 ? -1 : 1), 0));
 }
